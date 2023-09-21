ede740d7d9e24ebcc85502740e2005a0472676b8
bimamib <bimaprast1601@gmail.com>
2023-09-21 14:06:14 +0700

    akhir kelas NoteAdapter

--- app/src/main/java/com/bima/mynotesapp/adapter/NoteAdapter.kt
+++ app/src/main/java/com/bima/mynotesapp/adapter/NoteAdapter.kt
@@ -1,11 +1,15 @@
 package com.bima.mynotesapp.adapter
 
+import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
+import com.bima.mynotesapp.R
+import com.bima.mynotesapp.databinding.ItemNoteBinding
 import com.bima.mynotesapp.entity.Note
 
-class NoteAdapter(private val onItemClickCallback: OnItemClickCallback) : RecyclerView.Adapter<NoteAdapter.NoteViewHolder>() {
+class NoteAdapter(private val onItemClickCallback: OnItemClickCallback) :
+    RecyclerView.Adapter<NoteAdapter.NoteViewHolder>() {
     var listNotes = ArrayList<Note>()
         set(listNotes) {
             if (listNotes.size > 0) {
@@ -31,19 +35,26 @@ class NoteAdapter(private val onItemClickCallback: OnItemClickCallback) : Recycl
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): NoteViewHolder {
-
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.item_note, parent, false)
+        return NoteViewHolder(view)
     }
 
     override fun onBindViewHolder(holder: NoteViewHolder, position: Int) {
-
+        holder.bind(listNotes[position])
     }
 
-    override fun getItemCount(): Int {
-
-    }
+    override fun getItemCount(): Int = this.listNotes.size
 
     inner class NoteViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
-
+        private val binding = ItemNoteBinding.bind(itemView)
+        fun bind(note: Note) {
+            binding.tvItemTitle.text = note.title
+            binding.tvItemDate.text = note.date
+            binding.tvItemDescription.text = note.description
+            binding.cvItemNote.setOnClickListener {
+                onItemClickCallback.onItemClicked(note, adapterPosition)
+            }
+        }
     }
 
     interface OnItemClickCallback {
