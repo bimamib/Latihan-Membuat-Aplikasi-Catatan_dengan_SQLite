db58bb9692abdccbfb52bdcca9da86a97eda8680
bimamib <bimaprast1601@gmail.com>
2023-09-21 17:39:55 +0700

    mendeklarasikan variabelnya

--- app/src/main/java/com/bima/mynotesapp/NoteAddUpdateActivity.kt
+++ app/src/main/java/com/bima/mynotesapp/NoteAddUpdateActivity.kt
@@ -2,10 +2,65 @@ package com.bima.mynotesapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import com.bima.mynotesapp.databinding.ActivityNoteAddUpdateBinding
+import com.bima.mynotesapp.db.NoteHelper
+import com.bima.mynotesapp.entity.Note
 
-class NoteAddUpdateActivity : AppCompatActivity() {
+class NoteAddUpdateActivity : AppCompatActivity(), View.OnClickListener {
+
+    private var isEdit = false
+    private var note: Note? = null
+    private var position: Int = 0
+    private lateinit var noteHelper: NoteHelper
+
+    private lateinit var binding: ActivityNoteAddUpdateBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_note_add_update)
+        binding = ActivityNoteAddUpdateBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        noteHelper = NoteHelper.getInstance(applicationContext)
+        noteHelper.open()
+
+        note = intent.getParcelableExtra(EXTRA_NOTE)
+        if (note != null) {
+            position = intent.getIntExtra(EXTRA_POSITION, 0)
+            isEdit = true
+        } else {
+            note = Note()
+        }
+
+        val actionBarTitle: String
+        val btnTitle: String
+
+        if (isEdit) {
+            actionBarTitle = "Ubah"
+            btnTitle = "Update"
+
+            note?.let {
+                binding.edtTitle.setText(it.title)
+                binding.edtDescription.setText(it.description)
+            }
+
+        } else {
+            actionBarTitle = "Tambah"
+            btnTitle = "Simpan"
+        }
+
+        supportActionBar?.title = actionBarTitle
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+
+        binding.btnSubmit.text = btnTitle
     }
+
+    companion object {
+        const val EXTRA_NOTE = "extra_note"
+        const val EXTRA_POSITION = "extra_position"
+        const val RESULT_ADD = 101
+        const val RESULT_UPDATE = 201
+        const val RESULT_DELETE = 301
+        const val ALERT_DIALOG_CLOSE = 10
+        const val ALERT_DIALOG_DELETE = 20
 }
