3aac38d02a120129b3590e9cafdcf30439bbf5a9
bimamib <bimaprast1601@gmail.com>
2023-09-21 13:50:24 +0700

    membuat package baru dan class baru

---
+++ app/src/main/java/com/bima/mynotesapp/adapter/NoteAdapter.kt
@@ -0,0 +1,24 @@
+package com.bima.mynotesapp.adapter
+
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+
+class NoteAdapter : RecyclerView.Adapter<NoteAdapter.NoteViewHolder>() {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): NoteViewHolder {
+
+    }
+
+    override fun onBindViewHolder(holder: NoteViewHolder, position: Int) {
+
+    }
+
+    override fun getItemCount(): Int {
+
+    }
+
+    inner class NoteViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+
+    }
+}
