f6e7e27dda9e0ab6b3beb850e0e5070779615563
bimamib <bimaprast1601@gmail.com>
2023-09-21 17:52:36 +0700

    membuat metode getCurrentDate

--- app/src/main/java/com/bima/mynotesapp/NoteAddUpdateActivity.kt
+++ app/src/main/java/com/bima/mynotesapp/NoteAddUpdateActivity.kt
@@ -1,11 +1,21 @@
 package com.bima.mynotesapp
 
+import android.content.ContentValues
+import android.content.Intent
+import android.icu.text.SimpleDateFormat
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.Toast
+import androidx.annotation.RequiresApi
 import com.bima.mynotesapp.databinding.ActivityNoteAddUpdateBinding
+import com.bima.mynotesapp.db.DatabaseContract
+import com.bima.mynotesapp.db.DatabaseContract.NoteColumns.Companion.DATE
 import com.bima.mynotesapp.db.NoteHelper
 import com.bima.mynotesapp.entity.Note
+import java.util.Date
+import java.util.Locale
 
 class NoteAddUpdateActivity : AppCompatActivity(), View.OnClickListener {
 
@@ -15,6 +25,16 @@ class NoteAddUpdateActivity : AppCompatActivity(), View.OnClickListener {
     private lateinit var noteHelper: NoteHelper
 
     private lateinit var binding: ActivityNoteAddUpdateBinding
+
+    companion object {
+        const val EXTRA_NOTE = "extra_note"
+        const val EXTRA_POSITION = "extra_position"
+        const val RESULT_ADD = 101
+        const val RESULT_UPDATE = 201
+        const val RESULT_DELETE = 301
+        const val ALERT_DIALOG_CLOSE = 10
+        const val ALERT_DIALOG_DELETE = 20
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -53,14 +73,60 @@ class NoteAddUpdateActivity : AppCompatActivity(), View.OnClickListener {
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
 
         binding.btnSubmit.text = btnTitle
+
+        binding.btnSubmit.setOnClickListener(this)
     }
 
-    companion object {
-        const val EXTRA_NOTE = "extra_note"
-        const val EXTRA_POSITION = "extra_position"
-        const val RESULT_ADD = 101
-        const val RESULT_UPDATE = 201
-        const val RESULT_DELETE = 301
-        const val ALERT_DIALOG_CLOSE = 10
-        const val ALERT_DIALOG_DELETE = 20
+    @RequiresApi(Build.VERSION_CODES.N)
+    override fun onClick(view: View) {
+        if (view.id == R.id.btn_submit) {
+            val title = binding.edtTitle.text.toString().trim()
+            val description = binding.edtDescription.text.toString().trim()
+
+            if (title.isEmpty()) {
+                binding.edtTitle.error = "Field can not be blank"
+                return
+            }
+
+            note?.title = title
+            note?.description = description
+
+            val intent = Intent()
+            intent.putExtra(EXTRA_NOTE, note)
+            intent.putExtra(EXTRA_POSITION, position)
+
+            val values = ContentValues()
+            values.put(DatabaseContract.NoteColumns.TITLE, title)
+            values.put(DatabaseContract.NoteColumns.DESCRIPTION, description)
+
+            if (isEdit) {
+                val result = noteHelper.update(note?.id.toString(), values).toLong()
+                if (result > 0) {
+                    setResult(RESULT_UPDATE, intent)
+                    finish()
+                } else {
+                    Toast.makeText(this@NoteAddUpdateActivity, "Gagal mengupdate data", Toast.LENGTH_SHORT).show()
+                }
+            } else {
+                note?.date = getCurrentDate()
+                values.put(DATE, getCurrentDate())
+                val result = noteHelper.insert(values)
+
+                if (result > 0) {
+                    note?.id = result.toInt()
+                    setResult(RESULT_ADD, intent)
+                    finish()
+                } else {
+                    Toast.makeText(this@NoteAddUpdateActivity, "Gagal menambah data", Toast.LENGTH_SHORT).show()
+                }
+            }
+        }
+    }
+
+    @RequiresApi(Build.VERSION_CODES.N)
+    private fun getCurrentDate(): String {
+        val dateFormat = SimpleDateFormat("yyyy/MM/dd HH:mm:ss", Locale.getDefault())
+        val date = Date()
+        return dateFormat.format(date)
+    }
 }
