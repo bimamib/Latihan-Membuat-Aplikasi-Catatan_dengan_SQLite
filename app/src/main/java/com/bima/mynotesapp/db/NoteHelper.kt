843421dc49945acf2134df41acd7e2b9082685d1
bimamib <bimaprast1601@gmail.com>
2023-09-21 13:25:36 +0700

    membuat 1 metode untuk menginisiasi database

--- app/src/main/java/com/bima/mynotesapp/db/NoteHelper.kt
+++ app/src/main/java/com/bima/mynotesapp/db/NoteHelper.kt
@@ -5,10 +5,16 @@ import android.database.sqlite.SQLiteDatabase
 import com.bima.mynotesapp.db.DatabaseContract.NoteColumns.Companion.TABLE_NAME
 
 class NoteHelper(context: Context) {
-    private var databaseHelper: DatabaseHelper = DatabaseHelper(Context)
+    private var databaseHelper: DatabaseHelper = DatabaseHelper(context)
     private lateinit var database: SQLiteDatabase
 
     companion object {
         private const val DATABASE_TABLE = TABLE_NAME
+
+        private var INSTANCE: NoteHelper? = null
+        fun getInstance(context: Context): NoteHelper =
+            INSTANCE ?: synchronized(this) {
+                INSTANCE ?: NoteHelper(context)
+            }
     }
 }
